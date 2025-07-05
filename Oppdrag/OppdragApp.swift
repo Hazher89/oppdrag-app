dddcdd16f9c9d6e7c71ab6c49cf575c8aa4dbc3a
hazher <Hazhera@outlook.com>
2025-07-05 19:48:03 +0200

    Initial Commit

---
+++ Oppdrag/OppdragApp.swift
@@ -0,0 +1,17 @@
+//
+//  OppdragApp.swift
+//  Oppdrag
+//
+//  Created by Hazher  on 05/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct OppdragApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
